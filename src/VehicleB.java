466921a8f37724f9a5760da9e6408b95e060b563
Your Name <alo20172@uvg.edu.gt>
2022-11-18 07:13:21 -0600

    VehicleB

--- src/VehicleB.java
+++ src/VehicleB.java
@@ -1,3 +1,9 @@
 public class VehicleB extends Vehicle {
-    
+    private int lastContact;
+    public int getLastContact() {
+        return lastContact;
+    }
+    public void setLastContact(int lastContact) {
+        this.lastContact = lastContact;
+    }
 }
