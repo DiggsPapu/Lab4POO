b629d6f2b09cf454d7b2314ed447334476ab0d46
Your Name <alo20172@uvg.edu.gt>
2022-11-17 20:39:40 -0600

    IPhoneMOde

--- src/IStereo.java
+++ src/IStereo.java
@@ -1,13 +1,15 @@
-import java.util.ArrayList;
-
 public interface IStereo {
 public boolean getOnOff();
 public void setOnOff(boolean onOff);
 public int getVol();
 public void setVol(int vol);
 public String getMode();
-public void setMode(String mode);
-public ArrayList<String> getSetOfSomething();
-public void setSetOfSomething(ArrayList<String> array);
-    
+public void setMode(String mode);  
+public void changeSong(int set);
+public void callContact(int set);
+public int getPersonaInCall();
+public void setConnectPhone(boolean set);
+public boolean getInCall();
+public void setInCall(boolean set);
+
 } 
