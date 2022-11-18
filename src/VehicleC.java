387005613ac2b97d815943644c1315174d3f4a09
Your Name <alo20172@uvg.edu.gt>
2022-11-18 07:09:30 -0600

    Vehicle C

--- src/VehicleC.java
+++ src/VehicleC.java
@@ -1,3 +1,9 @@
 public class VehicleC extends Vehicle {
-    
+    private boolean speaker;
+    public boolean getSpeaker(){
+        return speaker;
+    }
+    public void setSpeaker(boolean speaker) {
+        this.speaker = speaker;
+    }
 }
