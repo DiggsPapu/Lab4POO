b649f24adadb04d0e881dcc90ec909636de2683f
Your Name <alo20172@uvg.edu.gt>
2022-11-17 20:02:05 -0600

    IStereo

---
+++ src/IStereo.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public interface IStereo {
+public boolean getOnOff();
+public void setOnOff(boolean onOff);
+public int getVol();
+public void setVol(int vol);
+public String getMode();
+public void setMode(String mode);
+public ArrayList<String> getSetOfSomething();
+public void setSetOfSomething(ArrayList<String> array);
+    
+} 
